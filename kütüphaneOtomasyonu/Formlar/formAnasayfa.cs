df8c54f109a35f2298022bb05e61f5cdd59dacc7
Mehmet Emin <meemin313@gmail.com>
2024-12-26 13:59:56 +0300

    Proje üzerined geliştirmeler yapıldı
    Üye ekle,üye listele, kitap türleri,kitap ekle, kitap listele, emanet kitaplar, iade kitaplar, çıkış butonları oluşturuldu. Bu butonlara basıldığında yeni form ekranına gitme için kullanılan gönderme kodları yazıldı. Gidilen form ekranlarındaki tasarımlar oluşturuldu.
    Text boxlara girilen bilgiler veri tabanına aktarılması için kullanılan kodlar yazıldı. ---- Kullannıcı adı ve şifre ekranı önceden oluşturulmuştu ama bi hata nedeniyele yeni yazılan kodlar o depoya gönderilemediği için yeni depo oluşturup tekrar gönderiyorum bilgilendirmeleri.

---
+++ kütüphaneOtomasyonu/Formlar/formAnasayfa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kütüphaneOtomasyonu.Formlar
+{
+    public partial class formAnasayfa : Form
+    {
+        public formAnasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUyeEkle_Click(object sender, EventArgs e)
+        {
+            frmUyeEkle frmUyeEkle = new frmUyeEkle();
+            frmUyeEkle.Show();
+            
+        }
+
+        private void btnUyeListele_Click(object sender, EventArgs e)
+        {
+            frmUyeListele frmUyeListele = new frmUyeListele();  
+            frmUyeListele.Show();
+        }
+
+        private void btnKitapTurleri_Click(object sender, EventArgs e)
+        {
+            kitaptürleri kitaptürleri = new kitaptürleri();
+            kitaptürleri.Show();
+        }
+
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            frmKitapEkle kitapEkle = new frmKitapEkle();
+            kitapEkle.Show();
+        }
+
+        private void btnKitapListele_Click(object sender, EventArgs e)
+        {
+            kitapListele kitapListele = new kitapListele(); 
+            kitapListele.Show();
+        }
+
+        private void btnEmanetKitaplar_Click(object sender, EventArgs e)
+        {
+            emanetKitaplar emanetKitaplar = new emanetKitaplar();
+            emanetKitaplar.Show();
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnİadeKitaplar_Click(object sender, EventArgs e)
+        {
+            iadeKitaplar iade = new iadeKitaplar();
+            iade.Show();
+        }
+    }
+}
